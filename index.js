4008b178f189d3d27c7de88f3e620d8dd2d014bb
Akrit Shrestha <akrit.act360@gmail.com>
2019-11-11 23:12:25 +0545

    bubble problem solved

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 window.addEventListener("load", () => {
   const sounds = document.querySelectorAll(".sound");
   const pads = document.querySelectorAll(".pads div");
-  const visual = document.querySelectorAll(".visual");
+  const visual = document.querySelector(".visual");
   const colors = [
     "#60d394",
     "#d36060",
     "#c060d3",
     "#d3d160",
-    "#6860d3",
-    "#60b2d3"
+    "#606bd3",
+    "#60c2d3"
   ];
 
-  //Sounds
-  pads.forEach((pad, i) => {
+  pads.forEach((pad, index) => {
     pad.addEventListener("click", function() {
-      sounds[i].currentTime = 0;
-      sounds[i].play();
-      createBubbles(i);
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+      createBubble(index);
     });
   });
 
-  //bubbles
-  const createBubbles = i => {
+  const createBubble = index => {
+    //Create bubbles
     const bubble = document.createElement("div");
     visual.appendChild(bubble);
-    bubble.style.backgroundColor = colors[i];
-    bubble.style.animation = "jump 1s ease";
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = `jump 1s ease`;
     bubble.addEventListener("animationend", function() {
       visual.removeChild(this);
     });
